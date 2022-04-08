ad6cbedf594baf692c0a8015651653a56923aed4
jflieder89 <99218360+jflieder89@users.noreply.github.com>
2022-04-07 21:21:50 -0400

    Add files via upload

---
+++ Distance between points 2D OOP.py
@@ -0,0 +1,29 @@
+﻿from math import hypot
+
+
+class Point:
+    def __init__(self, x=0.0, y=0.0):
+        self.__x = float(x)
+        self.__y = float(y)
+
+    def getx(self):
+        return self.__x
+
+    def gety(self):
+        return self.__y
+
+    def distance_from_xy(self, x, y):
+        return hypot( (self.__x - x), (self.__y - y))
+
+    def distance_from_point(self, point):
+        return hypot( (self.__x - point.__x), (self.__y - point.__y))
+
+
+point1 = Point(0, 0)
+point2 = Point(3, 4)
+# print(point1.getx())
+# print(point1.gety())
+# print(point2.getx())
+# print(point2.gety())
+print(point1.distance_from_point(point2))
+print(point2.distance_from_xy(2, 0))
---
+++ Triangle perimeter OOP.py
@@ -0,0 +1,55 @@
+﻿import math
+
+
+class Point:
+    def __init__(self, x=0.0, y=0.0):
+        self.__x = float(x)
+        self.__y = float(y)
+
+    def getx(self):
+        return self.__x
+
+    def gety(self):
+        return self.__y
+
+    def distance_from_xy(self, x, y):
+        return math.hypot( (self.__x - x), (self.__y - y))
+
+    def distance_from_point(self, point):
+        return math.hypot( (self.__x - point.__x), (self.__y - point.__y))
+
+
+class Triangle:
+    def __init__(self, vertex1, vertex2, vertex3):
+        self.__vertices = [vertex1, vertex2, vertex3]
+        self.__vertex1 = vertex1
+        self.__vertex2 = vertex2
+        self.__vertex3 = vertex3
+
+    def perimeter(self):
+        side_length_lst = []
+        
+        length0 = self.__vertices[0].distance_from_point(self.__vertices[1])
+        if length0 > 0: #if the length is positive or zero
+            side_length_lst.append(length0)
+        else: #if the length is negative
+            side_length_lst.append(length0 * (-1))
+        
+        length1 = self.__vertices[1].distance_from_point(self.__vertices[2])
+        if length1 > 0: #if the length is positive or zero
+            side_length_lst.append(length1)
+        else: #if the length is negative
+            side_length_lst.append(length1 * (-1))
+            
+        length2 = self.__vertices[1].distance_from_point(self.__vertices[0])
+        if length2 > 0: #if the length is positive or zero
+            side_length_lst.append(length2)
+        else: #if the length is negative
+            side_length_lst.append(length2 * (-1))
+            
+        return sum(side_length_lst)
+            
+
+
+triangle = Triangle(Point(0, 0), Point(1, 0), Point(0, 1))
+print(triangle.perimeter())
